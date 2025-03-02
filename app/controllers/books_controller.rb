09fafe8e88f1f0abe8e13e8d2dc0f5608e5afa0b
C0159412020 <adlightakinyi1@gmail.com>
2025-03-02 18:05:34 +0300

    generated controllers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:borrow, :return]
+  before_action :set_book, only: [:show, :borrow, :return]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def borrow
+    if @book.available?
+      current_user.borrowings.create(book: @book, due_date: 2.weeks.from_now, returned: false)
+      redirect_to user_path(current_user), notice: "Book borrowed successfully!"
+    else
+      redirect_to @book, alert: "Book is already borrowed."
+    end
+  end
+
+  def return
+    borrowing = current_user.borrowings.find_by(book: @book, returned: false)
+    if borrowing
+      borrowing.update(returned: true)
+      redirect_to user_path(current_user), notice: "Book returned successfully!"
+    else
+      redirect_to @book, alert: "You have not borrowed this book."
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "users/show"
+  get "books/index"
+  get "books/show"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
